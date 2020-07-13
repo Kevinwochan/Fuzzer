1a771b5022da865faf274f0c5b15b8cab9e93677
Kevin <kevinwochan@gmail.com>
2020-07-13 10:02:23 +1000

    fixing abstract class to be valid python

--- handler.py
+++ handler.py
@@ -1,4 +1,4 @@
-class handler(__init__):
+class handler():
     def __init__(self, sample_input):
         self.sample_input = sample_input
         self.byte_positions = self.sample_input_parser(sample_input)
@@ -8,7 +8,6 @@ class handler(__init__):
     
     def format_input(self):
         ''' stitches together mutated '''
-        for i in range(len(byte_positions)):
-            overflow_mutator(sample_input[i])
+        return ''
 
 
