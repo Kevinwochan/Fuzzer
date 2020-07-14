d252bcfe2c7b78701da27042c53022d20abec78e
Kevin <kevinwochan@gmail.com>
2020-07-15 04:10:31 +1000

    csvhandler now preserves empty lines

--- CsvHandler.py
+++ CsvHandler.py
@@ -1,51 +1,44 @@
-#from handler import handler
-from io import StringIO
-import csv
+from Handler import Handler
 import csv
 from io import StringIO
 
 
-class CsvHandler():
+class CsvHandler(Handler):
     buffer = StringIO()
-    def __init__(self, sample_input):
+    def __init__(self, sample_input_file_path):
         self.buffer.seek(0)
         self.buffer.truncate(0)
-        self.format = []
-        self.sample_input_parser(sample_input)
-        print(self.format)
+        self.sample = self.parse(sample_input_file_path)
 
-    def sample_input_parser(self, sample_input_file_path):
+    def data(self):
+        return self.sample
+
+    def parse(self, sample_input_file_path):
         ''' decomposes CSV into a 2-d list '''
+        rows = []
         csvfile = open(sample_input_file_path)
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
         csvfile.seek(0)
         parsed_sample = csv.reader(csvfile, dialect)
         for row in parsed_sample:
-            self.format.append(row)
+            rows.append(row)
+        return rows
 
     def decompose(self):
         ''' recursive call to decompose complex JSON '''
-        #TODO: cant handle complex JSON
         return
 
-    def get_sample(self):
-        ''' returns the array of flattenned input '''
-        return [format]
-    
-    def format_data(self, mutated_data):
+    def format(self, mutated_data):
         ''' uses the mutated_data array to construct a input of the same structure as the sample'''
         #assert len(mutated_data) == len(self.format) * len(self.format) TODO:sanity check
-        print(self.format)
         self.writer = csv.writer(self.buffer)
-        columns = len(self.format[0])
+        columns = len(self.sample[0])
         index = 0
-        for row in self.format:
+        empty_lines = 0
+        for row in self.sample:
+            if len(row) == 0:
+                empty_lines+=1
             self.writer.writerow(mutated_data[index:index + columns])
             index += columns
-        return self.buffer.getvalue()
+        return self.buffer.getvalue() + '\n'*empty_lines
             
-
-if __name__ == '__main__':
-    handler = CsvHandler('./files/csv1.txt')
-    mutated_csv = handler.format_data(['header', 'must', 'stay', 'intact', 'a', 'b', 'c', 'S', 'aa', 'b', 'c', 'S', 'i', 'j', 'k', 'et'])
-    print(mutated_csv)
