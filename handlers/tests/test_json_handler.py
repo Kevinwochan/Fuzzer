a5b3256389bf0096e0741b7fbd82ae7ffc936563
Kevin <kevinwochan@gmail.com>
2020-07-17 03:41:50 +1000

    implemented testing for json handler

--- handlers/tests/test_json_handler.py
+++ handlers/tests/test_json_handler.py
@@ -1,8 +1,18 @@
 import unittest
 import tempfile
 import json
-import os
 from handlers.json_handler import JsonHandler
+from mutators.base_mutator import BaseMutator
+
+
+class BadMutator(BaseMutator):
+    def __init__(self, input_str: str = ""):
+        super().__init__(input_str)
+
+    def mutate(self) -> str:
+        yield "A"
+        return
+
 
 class JsonHandlerTests(unittest.TestCase):
     """
@@ -11,13 +21,15 @@ class JsonHandlerTests(unittest.TestCase):
 
     def setUp(self):
         self.tmp_file = tempfile.NamedTemporaryFile(mode="w")
-        self.test_data = {"len":12, "input":"AAAABBBBCCCC", "more_data":["a","bb"]}
-        self.test_data_list = ["len",12, "input","AAAABBBBCCCC", "more_data", "a","bb"]
+        self.test_data_dict = {"len": 12, "input": "AAAABBBBCCCC", "more_data": ["a", "bb"]}
         self.test_data_raw = '{\n    "len": 12,\n    "input": "AAAABBBBCCCC",\n    "more_data": [\n        "a",\n        "bb"\n    ]\n}'
         # Write test data to temp file
         with open(self.tmp_file.name, "w") as tmp_json:
-            json.dump(self.test_data, tmp_json, indent=4) # indenting matches samples
+            json.dump(self.test_data_dict, tmp_json,
+                      indent=4)  # indenting matches samples
         self.handler = JsonHandler(sample_filename=self.tmp_file.name)
+        mutators = [BadMutator()]
+        self.handler.mutators = mutators
 
     def tearDown(self):
         self.tmp_file.close()
@@ -29,3 +41,19 @@ class JsonHandlerTests(unittest.TestCase):
 
         formatted = self.handler.data_raw
         self.assertEqual(formatted, self.test_data_raw)
+
+    def test_mutate_elem(self):
+        """
+        Tests that a primitive is correctly mutated
+        """
+        self.assertEqual(list(self.handler.mutate_elem("1")), ["A", "1"])
+
+    def test_generate_input(self):
+        """
+        Tests that a JSON is mutated in every field
+        """
+        mutated_set = (self.handler.mutate_structure(self.test_data_dict))
+        self.assertIn({"len": "A", "input": "AAAABBBBCCCC", "more_data": ["a", "bb"]}, mutated_set)
+        self.assertIn({"len": 12, "input": "AAAABBBBCCCC", "more_data": ["a", "bb"]}, mutated_set)
+        self.assertIn({"len": 12, "input": "AAAABBBBCCCC", "more_data": ["A", "bb"]}, mutated_set)
+        self.assertIn({"len": 12, "input": "AAAABBBBCCCC", "more_data": ["a", "A"]}, mutated_set)
