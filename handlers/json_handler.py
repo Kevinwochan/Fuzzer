1f114962fe362456f0a0eb173e0e4eb049bbcd8e
Kevin <kevinwochan@gmail.com>
2020-07-19 00:15:32 +1000

    remvoing return type defintion

--- handlers/json_handler.py
+++ handlers/json_handler.py
@@ -20,7 +20,7 @@ class JsonHandler(BaseHandler):
         self.mutators = [buf_overflow, fmt_str, rand_byte]
 
     @property
-    def mutators(self) -> list[BaseMutator]:
+    def mutators(self):
         return self._mutators
 
     @mutators.setter
