deec87735cf768a4f0529bd0e1f31c420de62db5
adamstucci <a.stucci@student.unsw.edu.au>
2020-08-08 00:41:30 +1000

    kill processes

--- fuzzer.py
+++ fuzzer.py
@@ -8,20 +8,22 @@ import multiprocessing.pool as mpp
 from pwn import process, log
 from io_controller import IoController
 import threading
-isDone = False
+#isDone = False#...pretty sure doing nothing
 
 
-def task(gen, ioController):
+def task(gen, ioController, queue):
     tac = timeit.default_timer()
     for val in gen:
-        global isDone
-        if isDone:
-            break
+        #global isDone
+        #if isDone:
+        #    break
         if ioController.run(val):
             toc = timeit.default_timer()
             print(f'Process: {toc-tac}')  
             print(f'Total: {toc-twac}')  
-            isDone = True
+            #isDone = True
+            #if task done, signal parent
+            queue.put(True)
             break
 
 if __name__ == "__main__":
@@ -47,17 +49,21 @@ if __name__ == "__main__":
     elif MULTI == "Yes":
         print("multi-process checkoutput")
         twac = timeit.default_timer()
-        for handler in handlers:
+        for handler in handlers: #would want to append the different handlers to our initially created processes rather than killing and creating new processes
             gen = handler.generate_input()
             alsdf = timeit.default_timer()
             inputs = list(gen)
             print(f"{timeit.default_timer() - alsdf} sdf")
             processes = []
             NUM_PROCS = mp.cpu_count()
+            queue = mp.Queue(2)
             for i in range(0, len(inputs), int(len(inputs)/NUM_PROCS)):
                 spl_inputs = inputs[i:i+int(len(inputs)/NUM_PROCS)]
-                p = mp.Process(target=task, args=(spl_inputs, ioController, ))
+                p = mp.Process(target=task, args=(spl_inputs, ioController, queue,))
                 processes.append(p)
                 p.start()
-            for process in processes:
-                process.join()
+            if queue.get():
+                for process in processes:
+                    if process.is_alive(): process.kill()
+                    #process.join()
+            queue.close()
