83d50299bfc12f1f49337e748826beba2db48061
Kevin <kevinwochan@gmail.com>
2020-08-10 20:50:29 +1000

    removing code used for debugging

--- fuzzer.py
+++ fuzzer.py
@@ -19,12 +19,13 @@ def task(gen, ioController, queue):
         #    break
         if ioController.run(val):
             toc = timeit.default_timer()
-            print(f'Process: {toc-tac}')  
-            print(f'Total: {toc-twac}')  
+            print(f'Process: {toc-tac}')
+            print(f'Total: {toc-twac}')
             #isDone = True
             #if task done, signal parent
             queue.put(True)
             break
+
 
 if __name__ == "__main__":
     BINARY = sys.argv[1]
@@ -35,8 +36,6 @@ if __name__ == "__main__":
     ioController = IoController(BINARY, FILENAME)
     handlers = ioController.get_handlers()
 
-    csv_handler = handlers[0]
-    gen = csv_handler.generate_input()
     if MULTI == "No":
         print("normal")
         start_single = timeit.default_timer()
@@ -44,12 +43,13 @@ if __name__ == "__main__":
             for input_str in handler.generate_input():
                 if ioController.run(input_str):
                     ioController.report_vuln(input_str)
-                    print(f'time taken: {timeit.default_timer() - start_single}')
+                    print(
+                        f'time taken: {timeit.default_timer() - start_single}')
                     exit()
     elif MULTI == "Yes":
         print("multi-process checkoutput")
         twac = timeit.default_timer()
-        for handler in handlers: #would want to append the different handlers to our initially created processes rather than killing and creating new processes
+        for handler in handlers:  #would want to append the different handlers to our initially created processes rather than killing and creating new processes
             gen = handler.generate_input()
             alsdf = timeit.default_timer()
             inputs = list(gen)
@@ -57,9 +57,14 @@ if __name__ == "__main__":
             processes = []
             NUM_PROCS = mp.cpu_count()
             queue = mp.Queue(2)
-            for i in range(0, len(inputs), int(len(inputs)/NUM_PROCS)):
-                spl_inputs = inputs[i:i+int(len(inputs)/NUM_PROCS)]
-                p = mp.Process(target=task, args=(spl_inputs, ioController, queue,))
+            for i in range(0, len(inputs), int(len(inputs) / NUM_PROCS)):
+                spl_inputs = inputs[i:i + int(len(inputs) / NUM_PROCS)]
+                p = mp.Process(target=task,
+                               args=(
+                                   spl_inputs,
+                                   ioController,
+                                   queue,
+                               ))
                 processes.append(p)
                 p.start()
             if queue.get():
