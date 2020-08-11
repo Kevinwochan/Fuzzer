7c56446a8d2aaf074c1b392f023e96e4facca590
Kevin <kevinwochan@gmail.com>
2020-08-11 14:24:21 +1000

    reverting super mutator to round robin

--- mutators/super_mutator.py
+++ mutators/super_mutator.py
@@ -6,44 +6,58 @@ from mutators.bufferoverflow_mutator import BufOverflowMutator
 from mutators.formatstring_mutator import FormatStringMutator
 from mutators.random_byte_mutator import RandomByteMutator
 from mutators.integeroverflow_mutator import IntOverflowMutator
+from mutators.duplicate_mutator import DuplicateMutator
+
+import time
 
 
 class SuperMutator():
+    timers = [0] * 10
     """
     mutator = Mutator("some_strings")
     mutated_str = mutator.mutate()
     """
-
     def __init__(self, input_str: str = ""):
-        self.init_mutators()
-        self.set_input_str(input_str)
-
-    def init_mutators(self) -> None:
         buf_overflow = BufOverflowMutator()
         fmt_str = FormatStringMutator()
         rand_byte = RandomByteMutator()
         int_overflow = IntOverflowMutator()
-        self.mutators = [fmt_str, rand_byte, int_overflow, buf_overflow ]
+        dup = DuplicateMutator()
+        self.mutators = [fmt_str, rand_byte, int_overflow, buf_overflow, dup]
+        self.set_input_str(input_str)
 
-    def is_empty(self) -> bool:
+    def is_empty(self):
         for mutator in self.mutators:
             if not mutator.is_empty:
                 return False
         return True
 
-    def mutate(self) -> str:
+    def ordered_mutate(self):
         """
-        Runs mutate() on all mutators.
+        Use this for measuring and debugging
         """
-        for mutator in self.mutators:
-            for mutated_str in mutator.mutate():
-                yield mutated_str
+        for index, mutator in enumerate(self.mutators):
+            start = time.time()
+            print(f'starting {type(mutator)}')
+            for mutated_data in mutator.mutate():
+                yield mutated_data
+            print(f'{type(mutator)} took {time.time() - start}s')
+            self.timers[index] += time.time() - start
+            print(f'{type(mutator)} total {self.timers[index]}')
+
+    def mutate(self):
+        """
+        A Generator that feeds on a round robin of mutators
+        """
+        generators = [mutator.mutate() for mutator in self.mutators]
+        while len(generators) > 0:
+            for index, generator in enumerate(generators):
+                mutated_data = next(generator, None)
+                if mutated_data is None:
+                    generators.pop(index)
+                    continue
+                yield mutated_data
 
     def set_input_str(self, input_str: str) -> None:
-        """
-        Sets input string for each mutator.
-        This function also resets all mutator generators.
-        """
-        self.init_mutators()
         for mutator in self.mutators:
             mutator.set_input_str(input_str)
