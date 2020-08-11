a5a9b6749a8dda2719b44d4ed7b65a5f6b15ecb7
Kevin <kevinwochan@gmail.com>
2020-08-11 16:56:44 +1000

    reverting to round robin

--- mutators/super_mutator.py
+++ mutators/super_mutator.py
@@ -13,37 +13,33 @@ class SuperMutator():
     mutator = Mutator("some_strings")
     mutated_str = mutator.mutate()
     """
-
     def __init__(self, input_str: str = ""):
-        self.init_mutators()
-        self.set_input_str(input_str)
-
-    def init_mutators(self) -> None:
         buf_overflow = BufOverflowMutator()
         fmt_str = FormatStringMutator()
         rand_byte = RandomByteMutator()
         int_overflow = IntOverflowMutator()
-        self.mutators = [fmt_str, rand_byte, int_overflow, buf_overflow ]
+        self.mutators = [fmt_str, rand_byte, int_overflow, buf_overflow]
+        self.set_input_str(input_str)
 
-    def is_empty(self) -> bool:
+    def is_empty(self):
         for mutator in self.mutators:
             if not mutator.is_empty:
                 return False
         return True
 
-    def mutate(self) -> str:
+    def mutate(self):
         """
-        Runs mutate() on all mutators.
+        A Generator that feeds on a round robin of mutators
         """
-        for mutator in self.mutators:
-            for mutated_str in mutator.mutate():
-                yield mutated_str
+        generators = [mutator.mutate() for mutator in self.mutators]
+        while len(generators) > 0:
+            for index, generator in enumerate(generators):
+                mutated_data = next(generator, None)
+                if mutated_data is None:
+                    generators.pop(index)
+                    continue
+                yield mutated_data
 
     def set_input_str(self, input_str: str) -> None:
-        """
-        Sets input string for each mutator.
-        This function also resets all mutator generators.
-        """
-        self.init_mutators()
         for mutator in self.mutators:
             mutator.set_input_str(input_str)
